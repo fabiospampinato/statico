24166c1de532fd28977900cc82d675a524d61abd
Fabio Spampinato <spampinabio@gmail.com>
2018-03-21 16:50:40 +0100

    Serverfy: implemented template

--- src/plugins/serverfy/template.js
+++ src/plugins/serverfy/template.js
@@ -1,10 +1,146 @@
 
-//TODO
+'use strict';
 
-function getRequests () {
+/* REQUIRE */
 
-  return /* REQUIRES:INJECT */;
+const _ = require ( 'lodash' );
+
+/* STARTUP */
+
+function startup () {
+
+  self.addEventListener ( 'install', () => self.skipWaiting () );
+  self.addEventListener ( 'activate', () => self.clients.claim () );
 
 }
 
-getRequests ();
+startup ();
+
+/* REQUESTS */
+
+function getRawRequests () {
+
+  return /* REQUIRES:INJECT */; // An array of raw requests objects will be injected here
+
+}
+
+function getRequests () {
+
+  const requestsRaw = getRawRequests () || [],
+        requests = _.merge ( {}, ...requestsRaw ),
+        requestsEnhanced = enhanceRequests ( requests );
+
+  return requestsEnhanced;
+
+}
+
+function enhanceRequests ( requests ) {
+
+  const defaultRequest = {
+    delay: 1000,
+    verbs: ['GET', 'POST']
+  };
+
+  return _.mapValues ( _.cloneDeep ( requests ), ( obj, url ) => {
+
+    if ( _.isPlainObject ( obj ) ) {
+
+      if ( obj.verb ) {
+
+        obj.verbs = _.castArray ( obj.verb );
+
+      }
+
+      if ( obj.url ) {
+
+        obj.urls = _.castArray ( obj.url );
+
+      }
+
+      if ( obj.urls ) {
+
+        obj.urls = _.flatten ( obj.urls.map ( url => [url, `${url}\\?*`] ) );
+
+      }
+
+    } else if ( _.isFunction ( obj ) ) {
+
+      obj = {
+        handler: obj
+      };
+
+    }
+
+    if ( !obj.urls ) {
+
+      obj.urls = [url, `${url}\\?*`];
+
+    }
+
+    return _.merge ( {}, defaultRequest, obj );
+
+  });
+
+}
+
+/* SERVICE WORKER WARE */
+
+function workerInit () {
+
+  require ( './sww' );
+
+  const worker = new ServiceWorkerWare ();
+
+  workerRequests ( worker );
+
+  worker.init ();
+
+}
+
+function workerRequests ( worker ) {
+
+  const requests = getRequests ();
+
+  _.forOwn ( requests, request => {
+
+    workerRequest ( worker, request );
+
+  });
+
+}
+
+function workerRequest ( worker, request ) {
+
+  const handler = workerHandler ( request );
+
+  request.verbs.forEach ( verb => {
+
+    verb = verb.toLowerCase ();
+
+    request.urls.forEach ( url => {
+
+      worker[verb].call ( worker, url, handler );
+
+    });
+
+  });
+
+}
+
+function workerHandler ( request ) {
+
+  return async function ( ...args ) {
+
+    await new Promise ( resolve => setTimeout ( resolve, request.delay ) );
+
+    const result = await request.handler ( ...args ),
+          resultStr = _.isPlainObject ( result ) ? JSON.stringify ( result, undefined, 2 ) : result,
+          response = new Response ( resultStr );
+
+    return response;
+
+  };
+
+}
+
+workerInit ();
