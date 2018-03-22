6ac896f950822bce7ff63bed7bc269974ae81092
Fabio Spampinato <spampinabio@gmail.com>
2018-03-22 11:28:02 +0100

    Static: copying over only newer files

--- src/tasks/build/static.js
+++ src/tasks/build/static.js
@@ -2,6 +2,7 @@
 /* REQUIRE */
 
 const gulp = require ( 'gulp' ),
+      newer = require ( 'gulp-newer' ),
       gutil = require ( '../../gutil' ),
       config = require ( '../../config' );
 
@@ -10,6 +11,7 @@ const gulp = require ( 'gulp' ),
 function task () {
 
   return gulp.src ( config.input.static, { dot: true, base: config.input.all } )
+             .pipe ( newer ( config.output.static ) )
              .pipe ( gulp.dest ( config.output.static ) );
 
 }
